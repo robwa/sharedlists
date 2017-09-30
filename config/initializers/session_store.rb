4c3de1c6a894de1d7a04684640aa1a942b6caf4b
wvengen <willem@thequestionmark.org>
2017-09-30 15:37:49 +0200

    Allow running on relative url root

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-SharedLists::Application.config.session_store :cookie_store, :key => '_sharedLists_session'
+SharedLists::Application.config.session_store :cookie_store, key: '_sharedLists_session', path: ENV['RAILS_RELATIVE_URL_ROOT'] || '/'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
